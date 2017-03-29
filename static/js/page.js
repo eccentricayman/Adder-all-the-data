03886efd86ad0575bcbd441d6043189abf0b445a
evookelj <evukelj@stuy.edu>
2017-03-29 18:43:10 -0400

    work in prog

--- static/js/page.js
+++ static/js/page.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-	//states = document.getElementsByTagName("path");
-	info = [1,2,3,4];
-	d3.selectAll("path").data(info)
-	d3.selectAll("path").text(function(d) { return d; })
+	var states = d3.select("svg").selectAll("path")['_groups'];
+	info = [1,2,3,4,5,6,7,8,9,10];
+	console.log(d3.select("svg").selectAll("path"))
+
+	d3.select("svg").selectAll("path")
+		.data(info)
+		.style("color", "pink");
+	console.log("hi")
 })
