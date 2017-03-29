ef554cf3a38ed27c45f9dcfcd58155f03a70c3c3
evookelj <evukelj@stuy.edu>
2017-03-29 19:07:29 -0400

    wooo map

--- static/js/page.js
+++ static/js/page.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-	var states = d3.select("svg").selectAll("path")['_groups'];
-	info = [1,2,3,4,5,6,7,8,9,10];
-	console.log(d3.select("svg").selectAll("path"))
 
-	d3.select("svg").selectAll("path")
+	var make_test = function(start, stop, step) {
+		var ret = [];
+		while (start<stop) {
+			ret.push(start);
+			start += step;
+		}
+		return ret;
+	}
+
+	var get_color = function(percent) {
+		 var val = "#00".concat(Math.round(percent*255).toString(16)).concat("00");
+		 console.log(val);
+		 return val;
+	}
+
+	var states = d3.select("svg").selectAll("path")['_groups'];
+	info = make_test(0,1,.02)
+	console.log(d3.selectAll("path"))
+
+	d3.selectAll("path")
 		.data(info)
-		.style("color", "pink");
-	console.log("hi")
+		.attr("fill", function(d) { return get_color(d); })
 })
