30b92be4d993ae1ee090676d453d05dba55efc21
evookelj <evukelj@stuy.edu>
2017-03-30 21:02:57 -0400

    Start on weighted avg school scores

--- utils/interact.py
+++ utils/interact.py
@@ -19,17 +19,30 @@ def convert_drugs():
 
 
 #--------------------------SCHOOL_SCORES---------------------------------
+let_to_num = {'A':4.0, 'B':3.0, 'C':2.0, 'D':1.0, 'F':0.1}
 
 def convert_scores():
-    lit = school_scores.get_all()
+    states = school_scores.get_all()
     ret = {}
-    for litty in lit:
-        ret[litty['State']['Name']] = litty
-        #print ret[litty['State']['Name']]
-        #print "\n\n"
+    for state in states: #FOR EACH STATE
+        weighted_data = []
+        for letter in state['GPA']: #FOR EACH GPA
+            if letter in let_to_num.keys():
+                if state['Year'] == '2014':
+                    math_pt = let_to_num[letter]*200 + int(state['GPA'][letter]['Math'])
+                    verbal_pt = let_to_num[letter]*200 + int(state['GPA'][letter]['Verbal'])
+                    if state['State']['Code']=='NY':
+                        print "SCORES %s: Math %d, Verbal %d"%(letter, math_pt, verbal_pt)
+                    for i in range(0,int(state['GPA'][letter]['Test-takers'])/2): #FOR EACH TEST TAKER
+                        # add average
+                        # this puts it on 0-100 which i like
+                        weighted_data.append(math_pt)
+                        weighted_data.append(verbal_pt)
+                    ret[state['State']['Code']] = weighted_data
     return ret
 '''
 def corr_coef(drugs, school):
     return numpy.corrcoef(drugs,school)[0,1]
 '''
-print convert_scores()['New York']['Score Ranges'].keys()
+
+convert_scores()['NY']
