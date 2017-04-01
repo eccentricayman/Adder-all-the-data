1c940d209049538eedf78b8cc206b13c6e8cd114
evookelj <evukelj@stuy.edu>
2017-04-01 11:42:40 -0400

    Modularized by year and state for getting corr coef yay

--- utils/interact.py
+++ utils/interact.py
@@ -62,20 +62,24 @@ state_codes = {
     'Guam': 'GU'
 }
 #---------------------------DRUG STUFF------------------------------------
-def convert_drugs(year):
-    states = drugs.get_reports()
-    ret = {}
-    for state in states:
-        #print "State: %s"%(state['State'])
-        if state['Year'] == year:
-            #ret[state_codes[state['State']]] = weighted_data
-            weighted_data = [state['Totals']['Marijuana']['Used Past Year']['12-17']*1000,
-                    state['Totals']['Pain Relievers Abuse Past Year']['12-17']*1000,
-                    state['Totals']['Illicit Drugs']['Dependence Past Year']['12-17']*1000,
-                    state['Totals']['Alcohol']['Dependence Past Year']['12-17']*1000]
-            # find stuff for diff rates, idk why theyre formatted diff :(
-            ret[state_codes[state['State']]] = weighted_data
-    return ret
+states_drugs = drugs.get_reports()
+
+def convert_drugs():
+    overall = {}
+    for year in range(2005,2015):
+        ret = {}
+        for state in states_drugs:
+            #print "State: %s"%(state['State'])
+            if state['Year'] == year:
+                #ret[state_codes[state['State']]] = weighted_data
+                weighted_data = [state['Totals']['Marijuana']['Used Past Year']['12-17']*1000,
+                        state['Totals']['Pain Relievers Abuse Past Year']['12-17']*1000,
+                        state['Totals']['Illicit Drugs']['Dependence Past Year']['12-17']*1000,
+                        state['Totals']['Alcohol']['Dependence Past Year']['12-17']*1000]
+                # find stuff for diff rates, idk why theyre formatted diff :(
+                ret[state_codes[state['State']]] = weighted_data
+        overall[year] = ret
+    return overall
 
 #print convert_drugs()['New York']
 #List of info given by each state (seperated into age groups -- we focus on 12-17)
@@ -85,22 +89,25 @@ def convert_drugs(year):
 
 #--------------------------SCHOOL_SCORES---------------------------------
 let_to_num = {'A':4.0, 'B':3.0, 'C':2.0, 'D':1.0, 'F':0.1}
+states_school = school_scores.get_all()
 
-def convert_scores(year):
-    states = school_scores.get_all()
-    ret = {}
-    for state in states: #FOR EACH STATE
-        weighted_data = []
-        for letter in state['GPA']: #FOR EACH GPA
-            if letter in let_to_num.keys() and state['Year'] == str(year):
-                pt = let_to_num[letter]*400 + int(state['GPA'][letter]['Math']) + int(state['GPA'][letter]['Verbal'])
-                for i in range(0,int(state['GPA'][letter]['Test-takers'])/2): #FOR EACH TEST TAKER
-                    # add average
-                    # this puts it on 0-100 which i like
-                    weighted_data.append(pt)
-                    #weighted_data.append(verbal_pt)
-                ret[state['State']['Code']] = weighted_data
-    return ret
+def convert_scores():
+    overall = {}
+    for year in range(2005,2015):
+        ret = {}
+        for state in states_school: #FOR EACH STATE
+            weighted_data = []
+            for letter in state['GPA']: #FOR EACH GPA
+                if letter in let_to_num.keys() and state['Year'] == str(year):
+                    pt = let_to_num[letter]*400 + int(state['GPA'][letter]['Math']) + int(state['GPA'][letter]['Verbal'])
+                    for i in range(0,int(state['GPA'][letter]['Test-takers'])/2): #FOR EACH TEST TAKER
+                        # add average
+                        # this puts it on 0-100 which i like
+                        weighted_data.append(pt)
+                        #weighted_data.append(verbal_pt)
+                    ret[state['State']['Code']] = weighted_data
+        overall[year] = ret
+    return overall
 
 def scale_data(school, drugs):
     drugs = sorted(drugs)
@@ -112,9 +119,10 @@ def scale_data(school, drugs):
         drugs.pop(len(drugs)-1)
     return drugs
 
-#convert_scores(2014)['NY']
-scores = convert_scores(2014)['NY']
-drugs =convert_drugs(2014)['NY']
-#print len(scores)
-drugs = scale_data(scores, drugs)
-print abs(numpy.corrcoef(scores,drugs)[0][1])
+def corr(year, state):
+    drugs = convert_drugs()[year][state]
+    scores = convert_scores()[year][state]
+    drugs = scale_data(scores, drugs)
+    return abs(numpy.corrcoef(scores,drugs)[0][1])
+
+print corr(2005, 'VA')
