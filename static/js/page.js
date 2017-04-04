1a4cc9f3e0da4a0f4b25655f283171763c71dcee
ChanYvonne <ychan1352@gmail.com>
2017-04-04 11:18:16 -0400

    add morning stuff

--- static/js/page.js
+++ static/js/page.js
@@ -3,46 +3,89 @@ var stateData;
 
 document.addEventListener("DOMContentLoaded", function(e) {
 
-	var get_color = function(percent) {
-		if ((percent==0) || (percent==null)) {
-			return "#ffffff";
-		}
-		var val = "#".concat(Math.round(percent*250).toString(16)).concat("ff").concat(Math.round(percent*250).toString(16))
-		//console.log(percent.toString().concat("\n").concat(val.toString()).concat("\n"));
-		return val;
+    var get_color = function(percent) {
+	if ((percent==0) || (percent==null)) {
+	    return "#ffffff";
 	}
+	var val = "#".concat(Math.round(percent*250).toString(16)).concat("ff").concat(Math.round(percent*250).toString(16))
+	//console.log(percent.toString().concat("\n").concat(val.toString()).concat("\n"));
+	return val;
+    }
 
-	$.ajax({
-		url: '/corrs/',
-		type: 'POST',
-		data: {},
-		success: function(i){
-			var info = JSON.parse(i)
-			keys = Object.keys(info)
+    $.ajax({
+	url: '/corrs/',
+	type: 'POST',
+	data: {},
+	success: function(i){
+	    var info = JSON.parse(i)
+	    keys = Object.keys(info)
 
-			var ctr = 0;
-			for (ctr=0; ctr<keys.length; ctr++) {
-				id = "#" + keys[ctr];
-				if (id=="#GA") {
-					console.log(info[keys[ctr]])
-				}
-				d3.selectAll(id)
-				.data([info[keys[ctr]]])
-				//.attr("fill", function(d) { return get_color(d); })
-			}
-
-			d3.selectAll("path")
-			.attr("fill", function(d) { 
-				return get_color(d);
-			})
-
-			d3.select("#path67")
-			.attr("fill", "none")
+	    var ctr = 0;
+	    for (ctr=0; ctr<keys.length; ctr++) {
+		id = "#" + keys[ctr];
+		if (id=="#GA") {
+		    console.log(info[keys[ctr]])
 		}
-	})
+		d3.selectAll(id)
+		    .data([info[keys[ctr]]])
+		//.attr("fill", function(d) { return get_color(d); })
+	    }
 
-    $.get("/nationalData", { }, function(d){
-        nationalData = d;
+	    d3.selectAll("path")
+		.attr("fill", function(d) { 
+		    return get_color(d);
+		})
+
+	    d3.select("#path67")
+		.attr("fill", "none")
+	}
+    })
+
+    $.ajax({
+	url: "/nationalData/",
+	type: 'GET',
+	data: {},
+	success: function(d){
+            var info = JSON.parse(d)
+	    keys = Object.keys(info)
+	    values = Object.values(info)
+
+	    var us = d3.select("id")//to be sorted out later)
+	    var stats = function() {
+		us.selectAll("div")
+		    .data(info)
+		    .enter()
+		    .append("div")
+		    .style("width", function(i){
+			return i*30 + "px";
+		    })
+		    .text( function(a){
+			return a;
+		    });
+	    }
+
+	    var transitionTest = function( scale ) {
+		us.selectAll("div")
+		    .data(info)
+		    .transition()
+		    .duration(2000)
+		    .style("width", function(b) {
+			return b * scale + "px";
+		    });
+	    };
+
+	    stats();
+	    transitionTest();
+	}
+    });
+
+    $.ajax({
+	url: "/stateData/",
+	type: 'GET',
+	data: {},
+	success: function(d){
+            var info = 
+	}
     });
     
 });
