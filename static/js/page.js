a34ffb5db9ff910ea9036c86101b41d422a7f1b5
FieryDarkWraith <kevinzhang3702@gmail.com>
2017-04-04 20:43:52 -0400

    made some more changes

--- static/js/page.js
+++ static/js/page.js
@@ -1,10 +1,8 @@
 var nationalData;
-var fullSVG;
-
-var map = jQuery.extend(true, {}, originalObject);
+var states;
 
 document.addEventListener("DOMContentLoaded", function(e) {
-
+    
     var get_color = function(percent) {
 	if ((percent==0) || (percent==null)) {
 	    return "#ffffff";
@@ -43,16 +41,69 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	}
     })
 
-
     $.get( "/nationalData", {}, function(d){
         nationalData = JSON.parse(d)
-	nationalKeys = Object.keys(nationalData)
-	nationalValues = Object.values(nationalData)
+	    nationalKeys = Object.keys(nationalData)
+	    nationalValues = Object.values(nationalData)
+    });
 
-	var us = d3.select("id")//??
-	
-	var stats = function() {
-	    us.selectAll("div")
+    states = document.getElementById('g5').children
+    for( i =0; i < states.length; i++){
+        if( states[i].getAttribute('id') != 'GA' || states[i].getAttribute('id') != 'DC' ){
+            states[i].addEventListener( "click", render ); 
+        };
+    };
+    
+});
+
+
+var render = function(){
+
+    //Loop through all the states, hiding them one by one
+    for( i=0; i < states.length; i++){
+        if( states[i].getAttribute('id') != this.getAttribute('id') ){
+            states[i].setAttribute("display", "none"); 
+        };
+    }
+
+    //Remove current eventListener
+    this.removeEventListener("click", render )
+
+    //Add new eventListener
+    this.addEventListener("click", reset )
+
+    //Move the state into proper spot. Also, add a Text Tag
+    this.setAttribute("transform", function(){
+        //Calculate Transformation
+
+        //First get Origin Point ( prob not the most accurate point to use. W/e )
+        var coords = this.getAttribute("d").split("l")[0].substring(1).split(',')//Get Rid of the M. I don't need it :D
+
+        var X = coords[0] 
+        var Y = coords[1]
+
+        var finalX = 50 // AYMAN *READ THIS*
+        var finalY = 50 // THESE COORDS ARE WHERE THE STATE WILL END UP. CHANGE AS NEED BE
+
+        return "("+finalX-X +","+finalY-Y+")"       
+    });
+    var svg = document.getElementById('us-map')
+    
+    
+}
+
+
+var reset = function(){
+    
+}
+
+
+
+/*
+var us = d3.select("id")//??
+
+var stats = function() {
+	us.selectAll("div")
 		.data(values)
 		.enter()
 		.append("div")
@@ -62,8 +113,8 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		.text( function(a){
 		    return a;
 		});
-	}
-	var transitionTest = function( scale ) {
+}
+var transitionTest = function( scale ) {
 	    us.selectAll("div")
 		.data(values)
 		.transition()
@@ -71,13 +122,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		.style("height", function(b) {
 		    return b * scale + "px";
 		});
-	};
-	
-	stats();
-	transitionTest();
-    });
+};
 
-    var renderData = function(d){
+var renderData = function(d){
 	var info = JSON.parse(d[0])
 	stateKeys = Object.keys(info)
 	stateValues = Object.values(info)
@@ -114,6 +161,4 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	transitionTest();
     };
 
-    
-});
-
+*/
